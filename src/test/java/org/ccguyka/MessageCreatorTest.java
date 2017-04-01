846b92a86ac68fa8aacdad505df5dcfa8c971ebd
ccguyka <ccguyka@gmail.com>
2017-04-01 14:22:36 +0200

    some additional tests for MessageCreator

--- src/test/java/org/ccguyka/MessageCreatorTest.java
+++ src/test/java/org/ccguyka/MessageCreatorTest.java
@@ -24,9 +24,37 @@ public class MessageCreatorTest {
     }
 
     @Test
-    public void verifyMininmalMessageIsCreated() {
-        Message message = messageCreator.from(new String[] {"-hk",  "foo", "-m" , "message"});
+    public void verifyMinimalMessageIsCreated() {
+        Message message = messageCreator.from(new String[] {"-hk",  "hook", "-m" , "message"});
 
-        assertThat(message).isEqualTo(new Message("foo", "message"));
+        assertThat(message).isEqualTo(new Message("hook", "message"));
+    }
+
+    @Test
+    public void verifyTitleIsSet() {
+        Message message = messageCreator.from(new String[] {"-hk",  "hook", "-m" , "message", "-t", "title"});
+
+        assertThat(message.getTitle()).isEqualTo("title");
+    }
+
+    @Test
+    public void verifyTitleLinkIsSet() {
+        Message message = messageCreator.from(new String[] {"-hk",  "hook", "-m" , "message", "-tl", "titlelink"});
+
+        assertThat(message.getTitleLink()).isEqualTo("titlelink");
+    }
+
+    @Test
+    public void verifyAuthorIsSet() {
+        Message message = messageCreator.from(new String[] {"-hk",  "hook", "-m" , "message", "-a", "author"});
+
+        assertThat(message.getAuthor()).isEqualTo("author");
+    }
+
+    @Test
+    public void verifyColorIsSet() {
+        Message message = messageCreator.from(new String[] {"-hk",  "hook", "-m" , "message", "-c", "green"});
+
+        assertThat(message.getColor()).isEqualTo("green");
     }
 }
