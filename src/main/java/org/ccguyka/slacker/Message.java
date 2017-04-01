f8c1d6b8a99a90d294261a8b15713ee97a17e16e
ccguyka <ccguyka@gmail.com>
2017-04-01 17:00:08 +0200

    add sonar integration

--- src/main/java/org/ccguyka/slacker/Message.java
+++ src/main/java/org/ccguyka/slacker/Message.java
@@ -60,8 +60,12 @@ public class Message {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Message message = (Message) o;
         return Objects.equal(destination, message.destination) &&
                 Objects.equal(messageText, message.messageText) &&
